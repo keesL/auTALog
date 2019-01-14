3bfac88a03edaa28da6a92121a73b3d2efeacddd
Kees Leune <kees@leune.org>
2019-01-14 15:00:22 -0500

    moved sensitive config to separate untracked file. Closed #2

--- auTALog/__init__.py
+++ auTALog/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
 from flask_security.utils import hash_password
+from auTALog import config
 import flask_security 
 
 
@@ -10,15 +11,9 @@ app = Flask(__name__)
 app.config['SECURITY_PASSWORD_SALT'] = 'unused'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['BOOTSTRAP_USE_MINIFIED'] = False
-# BEGIN FIXME  
-# this is somewhat acceptable which developing, but make sure
-# to take these settings out of the main source code repo and
-# move it to an untracked config file
 
-app.secret_key = b'M\xc8\x07\x02\xc7\xa4Y_\x99\x90\x0c\xf6r\xceY4'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://autalog:autalog@localhost/autalog'
-
-# END FIXME
+app.config['SQLALCHEMY_DATABASE_URI'] = config.config['db_uri']
+app.secret_key=config.config['secret_key']
 
 Bootstrap(app)
 db = SQLAlchemy(app)
@@ -31,18 +26,20 @@ from auTALog.models import User, Role
 user_datastore = flask_security.SQLAlchemyUserDatastore(db, User, Role)
 security = flask_security.Security(app, user_datastore)
 
+
 def setupDB():
-    adminRole = Role(name="admin", description="Admin role")
-    tutorRole = Role(name="tutor", description="Tutor")
-    graderRole = Role(name="grader", description="Grader")
+    with app.app_context():
+        adminRole = Role(name="admin", description="Admin role")
+        tutorRole = Role(name="tutor", description="Tutor")
+        graderRole = Role(name="grader", description="Grader")
 
-    adminUser = User(email="leune@adelphi.edu", password=hash_password('hello'))
+        adminUser = User(email="leune@adelphi.edu", password=hash_password('hello'))
 
-    db.session.add(adminUser)
-    db.session.add(adminRole)
-    db.session.add(tutorRole)
-    db.session.add(graderRole)
-    db.session.commit()
+        db.session.add(adminUser)
+        db.session.add(adminRole)
+        db.session.add(tutorRole)
+        db.session.add(graderRole)
+        db.session.commit()
 
-    ins = roles_users.insert().values(user_id=adminUser.id, role_id=adminRole.id)
-    db.session.execute(ins)
+        ins = roles_users.insert().values(user_id=adminUser.id, role_id=adminRole.id)
+        db.session.execute(ins)
